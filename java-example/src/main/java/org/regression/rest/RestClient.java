e8ef66a5ecbd275bc9b2085edebf36f2b4ce3b03
Patrik Horvath <ferencpatrikhorvath@gmail.com>
2024-06-06 22:57:58 +0200

    get request

--- java-example/src/main/java/org/regression/rest/RestClient.java
+++ java-example/src/main/java/org/regression/rest/RestClient.java
@@ -1,4 +1,34 @@
 package org.regression.rest;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpHeaders;
+
+import java.util.Map;
+
 public class RestClient {
+
+    private final RequestSpecification request;
+    private String baseUrl;
+
+
+    public RestClient(final String baseUrl, final Map<String, String> headerMap) {
+        this.baseUrl = baseUrl;
+        this.request = RestAssured
+                .given()
+                .baseUri(baseUrl)
+                .headers(headerMap)
+                .header(HttpHeaders.CONTENT_TYPE, "application/json");
+    }
+
+    private Response GET(final String endpoint) {
+        return request
+                .when()
+                .get(endpoint)
+                .then()
+                .extract()
+                .response();
+    }
+
 }
