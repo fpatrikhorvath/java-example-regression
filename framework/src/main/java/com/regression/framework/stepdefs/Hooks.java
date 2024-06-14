3d9d2abf4684126dff724c65c903a29081968baf
Patrik Horvath <ferencpatrikhorvath@gmail.com>
2024-06-14 16:10:57 +0200

    Dedicated runner for the UI and for the API tests

--- framework/src/main/java/com/regression/framework/stepdefs/Hooks.java
+++ framework/src/main/java/com/regression/framework/stepdefs/Hooks.java
@@ -7,6 +7,10 @@ import com.regression.framework.stores.ParabankPageStore;
 import com.regression.framework.stores.UserLayerContextStore;
 import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
 
 public class Hooks extends TestCore {
     private final WebDriverFactory webDriverFactory;
@@ -14,13 +18,17 @@ public class Hooks extends TestCore {
     public Hooks(final UserLayerContextStore userLayerContextStore,
                  final ScenarioContext scenarioContext,
                  final ParabankPageStore parabankPageStore,
-                 final WebDriverInitializationListener webDriverInitializationListener, final WebDriverFactory webDriverFactory) {
+                 final WebDriverFactory webDriverFactory) {
         super(userLayerContextStore, scenarioContext, parabankPageStore);
         this.webDriverFactory = webDriverFactory;
     }
 
     @After
     public void tearDown(final Scenario scenario) {
+        if (scenario.isFailed() && WebDriverInitializationListener.isInitialized()) {
+            byte[] screenshot = ((TakesScreenshot) webDriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", "Screenshot");
+        }
         if (WebDriverInitializationListener.isInitialized()) {
             webDriverFactory.tearDown();
             WebDriverInitializationListener.setIsInitialized(false);
