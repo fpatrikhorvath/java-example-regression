ecd924b5aec1b5a3459c1280bf470912d9516e38
Patrik Horvath <ferencpatrikhorvath@gmail.com>
2024-07-17 20:41:46 +0200

    added new tracking annotation, parser service impl, added and implemented allure cucumber7

--- framework/src/main/java/com/regression/framework/runner/UserInterfaceTestRunnerIT.java
+++ framework/src/main/java/com/regression/framework/runner/UserInterfaceTestRunnerIT.java
@@ -5,14 +5,14 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
-        features = {"classpath:features/user-layer"},
+        features = {"classpath:features/parabank"},
         glue = {"com.regression.framework.stepdefs"},
-        tags = "not @bug",
-        plugin = "html:target/report.html"
+        tags = "not @bug and not @flaking",
+        plugin = {"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 )
-public class ApiTestRunner extends AbstractTestNGCucumberTests {
+public class UserInterfaceTestRunnerIT extends AbstractTestNGCucumberTests {
     @Override
-    @DataProvider(parallel = true)
+    @DataProvider(parallel = false)
     public Object[][] scenarios() {
         return super.scenarios();
     }
--- framework/src/main/java/com/regression/framework/service/util/ParserService.java
+++ framework/src/main/java/com/regression/framework/service/util/ParserService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 @ScenarioScope
 public class ParserService {
-    public double parseDollar(final String balanceText){
+    public double parseDollar(final String balanceText) {
         return Double.parseDouble(balanceText.replace("$", ""));
     }
 }
